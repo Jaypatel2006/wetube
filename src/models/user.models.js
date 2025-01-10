2bf8670270543fd67935abb1c309fa1588e59021
Jay patel <jayspatel190506@gmail.com>
2025-01-10 21:06:43 +0530

    make user and video model

---
+++ src/models/user.models.js
@@ -0,0 +1,83 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+
+const userschema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowecase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowecase:true
+    },
+    fullname:{
+        type:String,
+        required:true
+    },
+    coverimage:{
+        type:String
+    },
+    watchhistory:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:Video
+        }
+    ],
+    password:{
+        type:String,
+        required:true
+    },
+    refreshtoken:{
+        type:String
+    }
+
+
+},{timestamps:true})
+
+userschema.pre("save",async function(){
+    if(!this.isModofied("password")) return next();
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userschema.methods.ispasscorrect = async function(pass){
+    await bcrypt.compare(pass,this.password);
+}
+
+userschema.methods.accesstokengenerate = function (){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username
+        },
+        process.env.access,
+        {
+            expiresIn:process.env.accessexp
+        }
+    )
+}
+
+userschema.methods.refreshtokengenerate = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username
+        },
+        process.env.refresh,
+        {
+            expiresIn:process.env.refreshexp
+        }
+    )
+}
+
+export const User = mongoose.model("User",userschema);
---
+++ src/models/video.moels.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const videoschema = new mongoose.Schema({
+    videofile:{
+        type:String,
+        required:true
+    },
+    thumbnail:{
+        type:String
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String
+    },
+    duration:{
+        type:Number
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    ispublished:{
+        type:Boolean,
+        default:true
+    },
+    creator:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+},{timestamps:true})
+
+videoschema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video",videoschema)
